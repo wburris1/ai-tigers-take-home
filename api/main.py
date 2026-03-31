28e0c30382606196a0143962d2b264f0eaeabba8
Wilder Burris-Wells <wburriswells@gmail.com>
2026-03-31 14:01:25 -0700

    updated middleware origins

--- api/main.py
+++ api/main.py
@@ -7,10 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
